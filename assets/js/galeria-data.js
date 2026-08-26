54e2b1751043e65efa1c093862d9356fb687042b
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2026-08-26 20:47:40 -0300

    correccion pie de galeria

--- assets/js/galeria-data.js
+++ assets/js/galeria-data.js
@@ -11,245 +11,245 @@ export const categoriasGaleria = [
 export const obrasGaleria = [
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_094210.jpg",
-    "titulo": "\"20251025 094210\"",
+    "titulo": "\"Casitas de muñecas\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_094220.jpg",
-    "titulo": "\"20251025 094220\"",
+    "titulo": "\"Casitas de muñecas\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121412.jpg",
-    "titulo": "\"20251025 121412\"",
+    "titulo": "\"Casitas de muñecas\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121445.jpg",
-    "titulo": "\"20251025 121445\"",
+    "titulo": "\"Casitas de muñecas\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121455.jpg",
-    "titulo": "\"20251025 121455\"",
+    "titulo": "\"Casitas de muñecas\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250717_190843.jpg",
-    "titulo": "\"20250717 190843\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250725_200159.jpg",
-    "titulo": "\"20250725 200159\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250913_111313.jpg",
-    "titulo": "\"20250913 111313\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194300.jpg",
-    "titulo": "\"20260605 194300\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194304.jpg",
-    "titulo": "\"20260605 194304\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194309.jpg",
-    "titulo": "\"20260605 194309\"",
+    "titulo": "\"Manitos en acción\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20250813_203909.jpg",
-    "titulo": "\"20250813 203909\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260210_100836.jpg",
-    "titulo": "\"20260210 100836\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260717_200225.jpg",
-    "titulo": "\"20260717 200225\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260717_201245.jpg",
-    "titulo": "\"20260717 201245\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260722_105630.jpg",
-    "titulo": "\"20260722 105630\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/IMG_20230729_120233332.jpg",
-    "titulo": "\"IMG 20230729 120233332\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/IMG_20230819_103617972_BURST000_COVER_TOP.jpg",
-    "titulo": "\"IMG 20230819 103617972 BURST000 COVER TOP\"",
+    "titulo": "\"Manualiades\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182111.jpg",
-    "titulo": "\"20260813 182111\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182201.jpg",
-    "titulo": "\"20260813 182201\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182254.jpg",
-    "titulo": "\"20260813 182254\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182350.jpg",
-    "titulo": "\"20260813 182350\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182657.jpg",
-    "titulo": "\"20260813 182657\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/IMG_20240131_203529_064_LL.jpg",
-    "titulo": "\"IMG 20240131 203529 064 LL\"",
+    "titulo": "\"Muñecos y personajes\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20251029_181612.jpg",
-    "titulo": "\"20251029 181612\"",
+    "titulo": "\"Nuestras fiestas!\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20260404_170634.jpg",
-    "titulo": "\"20260404 170634\"",
+    "titulo": "\"Nuestras fiestas!\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20260708_165629.jpg",
-    "titulo": "\"20260708 165629\"",
+    "titulo": "\"Nuestras fiestas!\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/IMG-20231227-WA0020.jpg",
-    "titulo": "\"IMG 20231227 WA0020\"",
+    "titulo": "\"Nuestras fiestas!\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/IMG_20231031_185411_231_PORTRAIT.jpg",
-    "titulo": "\"IMG 20231031 185411 231 PORTRAIT\"",
+    "titulo": "\"Nuestras fiestas!\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/pintura/20250801_200303.jpg",
-    "titulo": "\"20250801 200303\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/20260813_173958.jpg",
-    "titulo": "\"20260813 173958\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/20260814_195354.jpg",
-    "titulo": "\"20260814 195354\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/2e2947c3-281b-45d8-8c60-db3224a16850~1.jpg",
-    "titulo": "\"2e2947c3 281b 45d8 8c60 Db3224a16850~1\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/IMG_20240115_165227193.jpg",
-    "titulo": "\"IMG 20240115 165227193\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/IMG_20240123_195510_038_LL.jpg",
-    "titulo": "\"IMG 20240123 195510 038 LL\"",
+    "titulo": "\"Pintura\"",
     "autor": "COMPLETAR nombre (edad)",
     "categoria": "pintura",
     "etiqueta": "pintura"
