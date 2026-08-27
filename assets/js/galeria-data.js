209c5e76d22f0bb4dc2c4b27710c0f8ad21462c4
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2026-08-27 14:26:27 -0300

    correccion pie de galeria

--- assets/js/galeria-data.js
+++ assets/js/galeria-data.js
@@ -12,245 +12,245 @@ export const obrasGaleria = [
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_094210.jpg",
     "titulo": "\"Casitas de muñecas\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_094220.jpg",
     "titulo": "\"Casitas de muñecas\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121412.jpg",
     "titulo": "\"Casitas de muñecas\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121445.jpg",
     "titulo": "\"Casitas de muñecas\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/casitas-de-munecas/20251025_121455.jpg",
     "titulo": "\"Casitas de muñecas\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "casitas-de-munecas",
     "etiqueta": "casitas de munecas"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250717_190843.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250725_200159.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20250913_111313.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194300.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194304.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manitos-en-accion/20260605_194309.jpg",
     "titulo": "\"Manitos en acción\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manitos-en-accion",
     "etiqueta": "manitos en accion"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20250813_203909.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260210_100836.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260717_200225.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260717_201245.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/20260722_105630.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/IMG_20230729_120233332.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/manualidades/IMG_20230819_103617972_BURST000_COVER_TOP.jpg",
     "titulo": "\"Manualiades\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "manualidades",
     "etiqueta": "manualidades"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182111.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182201.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182254.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182350.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/20260813_182657.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/munecos-y-personajes/IMG_20240131_203529_064_LL.jpg",
     "titulo": "\"Muñecos y personajes\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "munecos-y-personajes",
     "etiqueta": "munecos y personajes"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20251029_181612.jpg",
     "titulo": "\"Nuestras fiestas!\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20260404_170634.jpg",
     "titulo": "\"Nuestras fiestas!\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/20260708_165629.jpg",
     "titulo": "\"Nuestras fiestas!\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/IMG-20231227-WA0020.jpg",
     "titulo": "\"Nuestras fiestas!\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/nuestras-fiestas/IMG_20231031_185411_231_PORTRAIT.jpg",
     "titulo": "\"Nuestras fiestas!\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "nuestras-fiestas",
     "etiqueta": "nuestras fiestas"
   },
   {
     "imagen": "assets/img/galeria/pintura/20250801_200303.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/20260813_173958.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/20260814_195354.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/2e2947c3-281b-45d8-8c60-db3224a16850~1.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/IMG_20240115_165227193.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   },
   {
     "imagen": "assets/img/galeria/pintura/IMG_20240123_195510_038_LL.jpg",
     "titulo": "\"Pintura\"",
-    "autor": "COMPLETAR nombre (edad)",
+    "autor": "🎨 Taller de arte infantil!",
     "categoria": "pintura",
     "etiqueta": "pintura"
   }
